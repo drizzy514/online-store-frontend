d061ae6bf6c11425d2a018efebe9344fca774d30
abduhakim <78894135+drizzy514@users.noreply.github.com>
2021-12-18 22:00:06 +0500

    Update Auth.js

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -40,17 +40,17 @@ const Auth = observer(() => {
             style={{height: window.innerHeight - 54}}
         >
             <Card style={{width: 600}} className="p-5">
-                <h2 className="m-auto">{isLogin ? 'Авторизация' : "Регистрация"}</h2>
+                <h2 className="m-auto">{isLogin ? 'Login' : "Registration"}</h2>
                 <Form className="d-flex flex-column">
                     <Form.Control
                         className="mt-3"
-                        placeholder="Введите ваш email..."
+                        placeholder="your email..."
                         value={email}
                         onChange={e => setEmail(e.target.value)}
                     />
                     <Form.Control
                         className="mt-3"
-                        placeholder="Введите ваш пароль..."
+                        placeholder="your password..."
                         value={password}
                         onChange={e => setPassword(e.target.value)}
                         type="password"
@@ -58,18 +58,18 @@ const Auth = observer(() => {
                     <Row className="d-flex justify-content-between mt-3 pl-3 pr-3">
                         {isLogin ?
                             <div>
-                                Нет аккаунта? <NavLink to={REGISTRATION_ROUTE}>Зарегистрируйся!</NavLink>
+                               No account? <NavLink to={REGISTRATION_ROUTE}>Registration!</NavLink>
                             </div>
                             :
                             <div>
-                                Есть аккаунт? <NavLink to={LOGIN_ROUTE}>Войдите!</NavLink>
+                                 have an account? <NavLink to={LOGIN_ROUTE}>Login!</NavLink>
                             </div>
                         }
                         <Button
                             variant={"outline-success"}
                             onClick={click}
                         >
-                            {isLogin ? 'Войти' : 'Регистрация'}
+                            {isLogin ? 'Login' : 'Registration'}
                         </Button>
                     </Row>
 
