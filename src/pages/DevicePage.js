bc7771c841e407394c3b6d6d01322f17773dfbdc
abduhakim <78894135+drizzy514@users.noreply.github.com>
2021-12-18 22:02:05 +0500

    Update DevicePage.js

--- src/pages/DevicePage.js
+++ src/pages/DevicePage.js
@@ -34,12 +34,12 @@ const DevicePage = () => {
                         style={{width: 300, height: 300, fontSize: 32, border: '5px solid lightgray'}}
                     >
                         <h3>От: {device.price} руб.</h3>
-                        <Button variant={"outline-dark"}>Добавить в корзину</Button>
+                        <Button variant={"outline-dark"}>Cart</Button>
                     </Card>
                 </Col>
             </Row>
             <Row className="d-flex flex-column m-3">
-                <h1>Характеристики</h1>
+                <h1>Characteristics</h1>
                 {device.info.map((info, index) =>
                     <Row key={info.id} style={{background: index % 2 === 0 ? 'lightgray' : 'transparent', padding: 10}}>
                         {info.title}: {info.description}
