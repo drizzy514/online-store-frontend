8024f07d1b35571a9621bdae1457302b6e71d34c
abduhakim <78894135+drizzy514@users.noreply.github.com>
2021-12-27 19:49:46 +0500

    Update CreateType.js

--- src/components/modals/CreateType.js
+++ src/components/modals/CreateType.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import Modal from "react-bootstrap/Modal";
 import {Form, Button} from "react-bootstrap";
+import {createType} from "../../http/deviceAPI";
 
 const CreateType = ({show, onHide}) => {
     const [value, setValue] = useState('')
