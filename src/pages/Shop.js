e660fe39471251939a4454f9f3583abd5e092090
abduhakim <78894135+drizzy514@users.noreply.github.com>
2021-12-27 19:38:00 +0500

    Update Shop.js

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -21,12 +21,12 @@ const Shop = observer(() => {
         })
     
 
-    useEffect(() => {
+   
         fetchDevices(device.selectedType.id, device.selectedBrand.id, device.page, 2).then(data => {
             device.setDevices(data.rows)
             device.setTotalCount(data.count)
         })
-    }, [device.page, device.selectedType, device.selectedBrand,])
+    
 
     return (
         <Container>
