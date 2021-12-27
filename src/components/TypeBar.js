17ac276d2b5e1515ead10c7f573dfe6442c5bcfd
abduhakim <78894135+drizzy514@users.noreply.github.com>
2021-12-27 19:30:26 +0500

    Update TypeBar.js

--- src/components/TypeBar.js
+++ src/components/TypeBar.js
@@ -1,7 +1,6 @@
 import React, {useContext} from 'react';
 import {observer} from "mobx-react-lite";
 import {Context} from "../index";
-import Col from "react-bootstrap/Col";
 import ListGroup from "react-bootstrap/ListGroup";
 
 const TypeBar = observer(() => {
